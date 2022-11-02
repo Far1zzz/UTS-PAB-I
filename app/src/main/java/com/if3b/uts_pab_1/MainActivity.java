bc0605d9a165e7e09987e1dc2fcc4e8a87b0ddd4
farizz <fairizwalker@gmail.com>
2022-11-02 18:29:15 +0700

    final

---
+++ app/src/main/java/com/if3b/uts_pab_1/MainActivity.java
@@ -0,0 +1,61 @@
+package com.if3b.uts_pab_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnDaftar;
+    EditText etNamaLengkap;
+    EditText etNoPendaftaran;
+    CheckBox cbConfirm;
+    Spinner spJalurPendaftaran;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnDaftar = findViewById(R.id.btn_daftar);
+        etNamaLengkap = findViewById(R.id.et_namaLengkap);
+        etNoPendaftaran = findViewById(R.id.et_noPendaftaran);
+        cbConfirm = findViewById(R.id.cb_konfirm);
+        spJalurPendaftaran = findViewById(R.id.sp_jalurPendaftaran);
+
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String namaLengkap  = etNamaLengkap.getText().toString();
+                String noPendaftaran = etNoPendaftaran.getText().toString();
+                String cbKonfir = cbConfirm.getText().toString();
+                String sp = String.valueOf(spJalurPendaftaran.getSelectedItem());
+
+                if (namaLengkap.trim().equals("")){
+                    etNamaLengkap.setError("Nama Harus Di Isi!!!");
+                }
+                else if(noPendaftaran.trim().equals("")){
+                    etNoPendaftaran.setError("Nomor Harus Di isi!!!");
+                }
+                else if(!cbConfirm.isChecked()){
+                    Toast.makeText(MainActivity.this, "Check Box Belom Di Centang", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent geser = new Intent(MainActivity.this, MainActivity2.class);
+                    geser.putExtra("xnama", namaLengkap);
+                    geser.putExtra("noDaftar", noPendaftaran);
+                    geser.putExtra("confirm", cbKonfir);
+                    geser.putExtra("spJP", sp);
+                    startActivity(geser);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/if3b/uts_pab_1/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.if3b.uts_pab_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView tv_Nama;
+    TextView tv_NoPendaftaran;
+    TextView tv_JalurPendaftaran;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tv_Nama = findViewById(R.id.tv_nama);
+        tv_NoPendaftaran = findViewById(R.id.tv_noPendaftaran);
+        tv_JalurPendaftaran = findViewById(R.id.tv_jalurPendaftaran);
+        Intent acc = getIntent();
+        String aNama = acc.getStringExtra("xnama");
+        tv_Nama.setText(aNama);
+        String noPendaftar = acc.getStringExtra("noDaftar");
+        tv_NoPendaftaran.setText(noPendaftar);
+        String spin = acc.getStringExtra("spJP");
+        tv_JalurPendaftaran.setText(spin);
+
+
+
+
+    }
+}
